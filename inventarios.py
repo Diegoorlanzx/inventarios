7de328c56047e53754ff2f3c076ad077d759828a
Diegoorlanzx <155659029+Diegoorlanzx@users.noreply.github.com>
2025-06-18 16:43:02 -0500

    Actualización:mejoras en sitema de productos

--- inventarios.py
+++ inventarios.py
@@ -10,6 +10,26 @@ for i in range(numero_productos):
     precio = float(input(('Precio : ')))
     cantidad = int(input('Cantidad : '))
 
-    producto = {'id' : i+1, 'nombre' : {nombre}, 'precio' : {precio}, 'cantidad' : {cantidad}}
+    producto = {
+        'id': i + 1,
+        'nombre': nombre,
+        'precio': precio,
+        'cantidad': cantidad
+    }
     inventario.append(producto)
+    print(f'\n')
 print(f'\nInventario inicial : \n {inventario}')
+
+buscar= int(input(f'\n ingrese el valor a buscar'))
+producto_encontrado= None
+
+for producto in inventario:
+    if producto.get('id')== buscar:
+        producto_encontrado=producto
+
+        break
+    if producto_encontrado != None:
+        print(f'\n producto encontrado :\n {producto}')
+        break
+    else :
+        print(f'\n el producto {buscar} no fue encontrado')
